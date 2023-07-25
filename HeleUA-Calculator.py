61b90639189b00c04f14cf9e1f23258dbf132397
heleidan <ceduardo.avila@alumnos.udg.mx>
2023-07-24 21:31:14 -0600

    final commit push to production

--- HeleUA-Calculator.py
+++ HeleUA-Calculator.py
@@ -2,7 +2,7 @@ from socket import SOMAXCONN
 import tkinter as tk
 LARGE_FONT_STYLE = ("Arial", 40, "bold")
 SMALL_FONT_STYLE = ("Arial", 16)
-DIGITS_FONT_STYLE = ("Arial", 24)
+DIGITS_FONT_STYLE = ("Arial", 20)
 DEFAULT_FONT_STYLE = ("Arial", 20)
 
 OFF_WHITE = "#F8FAFF"
@@ -17,9 +17,11 @@ class Calculator:
         self.window.geometry("375x547")
         self.window.resizable(0, 0)
         self.window.title("HeleUA Calculator")
+        #icon
+        self.window.iconbitmap(default='main_icon.ico')
         #Display Label 100%
-        self.total_expression = "0"
-        self.current_expression = "0"
+        self.total_expression = ""
+        self.current_expression = ""
         self.display_frame = self.create_display_frame()
         self.total_label, self.label= self.create_display_labels()
         #digits 100%
@@ -39,10 +41,19 @@ class Calculator:
         self.create_digit_buttons()
         self.create_operation_buttons()
         self.create_special_buttons()
-    #definition of display label function 100%
+        self.bind_keys()
+    #to use computer keyboard numbers
+    def bind_keys(self):
+        self.window.bind("<Return>", lambda event: self.evaluate())
+        for key in self.digits:
+            self.window.bind(str(key), lambda event,digit=key: self.add_to_expression(digit))
+        for key in self.operations:
+            self.window.bind(key, lambda event, operator=key: self.append_operator(operator))   
     def create_special_buttons(self):
         self.create_clear_button()
         self.create_equals_button()
+        self.create_square_button()
+        self.create_sqrt_button()   
     def create_display_labels(self):
         total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, 
         bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
@@ -58,26 +69,71 @@ class Calculator:
         frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
         frame.pack(expand = True, fill='both')
         return frame
+    def add_to_expression(self, value):
+        self.current_expression += str(value)
+        self.update_label()
     def create_digit_buttons(self):
         for digit,grid_value in self.digits.items():
-            button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE, borderwidth=0)
+            button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR,font=DIGITS_FONT_STYLE, borderwidth=0, command=lambda x=digit: self.add_to_expression(x))
             button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+    def append_operator(self, operator):
+        self.current_expression += operator
+        self.total_expression += self.current_expression
+        self.current_expression = ""
+        self.update_total_label()
+        self.update_label()
     def create_operation_buttons(self):
         i = 0
         for operator, symbol in self.operations.items():
-            button = tk.Button(self.buttons_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+            button = tk.Button(self.buttons_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0, command=lambda y=operator: self.append_operator(y))
             button.grid(row=i, column=4, sticky=tk.NSEW)
             i+=1
+    def clear(self):
+        self.current_expression = ""
+        self.total_expression = ""
+        self.update_label()
+        self.update_total_label()
     def create_clear_button(self):
-        button = tk.Button(self.buttons_frame, text="C", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
-        button.grid(row=0, column=1, columnspan= 3, sticky=tk.NSEW)
+        button = tk.Button(self.buttons_frame, text="CE", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0, command=self.clear)
+        button.grid(row=0, column=1, sticky=tk.NSEW)
+    def square(self):
+        self.current_expression = str(eval(f"{self.current_expression}**2"))
+        self.update_label()
+    def create_square_button(self):
+        button = tk.Button(self.buttons_frame, text="x\u00b2", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0, command=self.square)
+        button.grid(row=0, column=2, sticky=tk.NSEW)
+    def sqrt(self):
+        self.current_expression = str(eval(f"{self.current_expression}**0.5"))
+        self.update_label()
+    def create_sqrt_button(self):
+        button = tk.Button(self.buttons_frame, text="\u221ax", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0, command=self.sqrt)
+        button.grid(row=0, column=3, sticky=tk.NSEW)    
+    def evaluate(self):
+        self.total_expression += self.current_expression
+        self.update_total_label()
+        try:
+            self.current_expression = str(eval(self.total_expression))
+            self.total_expression = ""
+        except Exception as e:
+            self.current_expression = "Error"
+        finally:
+            self.update_label()
+        
     def create_equals_button(self):
-        button = tk.Button(self.buttons_frame, text="=", bg=LIGHT_BLUE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0)
+        button = tk.Button(self.buttons_frame, text="=", bg=LIGHT_BLUE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE, borderwidth=0, command=self.evaluate)
         button.grid(row=4, column=3, columnspan= 32, sticky=tk.NSEW)
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
         frame.pack(expand=True, fill='both')
-        return frame
+        return frame 
+    def update_total_label(self):
+        expression = self.total_expression
+        for operator, synbol in self.operations.items():
+            expression = expression.replace(operator, f' {synbol} ') #replace symbols in display_frame
+        self.total_label.config(text=expression)
+
+    def update_label(self):
+        self.label.config(text=self.current_expression[:11]) #prevent string overflow
 
     def run(self): 
         self.window.mainloop()
@@ -87,4 +143,4 @@ if __name__ == "__main__":
 
 
     
-#Christian bebe lvl 1
+#Pyinstaller.exe --onefile --windowed --icon=app.ico app.py
--- README.md
+++
@@ -1,2 +0,0 @@
-# GUI-Calculator-in-Python3
-HeleUA its an actual calculator build from scratch using python and tkinter, more additions can be implemented  
